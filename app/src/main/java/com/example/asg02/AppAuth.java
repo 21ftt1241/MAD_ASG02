ac2e01f2129cf264f6c28d894d521779472d8572
Maxi <21FTT1245@student.pb.edu.bn>
2023-04-27 23:57:44 +0800

    Update
    added registration page. Logic still needed

--- app/src/main/java/com/example/asg02/AppAuth.java
+++ app/src/main/java/com/example/asg02/AppAuth.java
@@ -2,13 +2,24 @@ package com.example.asg02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AppAuth extends AppCompatActivity {
+
+    String username = "";
+    String password = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appauth);
     }
+
+    public void register(View view){
+        Intent i = new Intent(this, RegisterActivity.class);
+        startActivity(i);
+    }
+
 }
---
+++ app/src/main/java/com/example/asg02/RegisterActivity.java
@@ -0,0 +1,19 @@
+package com.example.asg02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+    }
+
+    public void registerAccount(View view) {
+
+    }
+}
