6ec025c082721433b2921cbbed0afdc517034da2
Maxi <21FTT1245@student.pb.edu.bn>
2023-04-28 00:51:57 +0800

    Login/Registration
    Login/Registration is completed.

--- app/src/main/java/com/example/asg02/AppAuth.java
+++ app/src/main/java/com/example/asg02/AppAuth.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AppAuth extends AppCompatActivity {
 
-    String username = "";
-    String password = "";
+    static String username = "";
+    static String password = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,22 @@ public class AppAuth extends AppCompatActivity {
     public void register(View view){
         Intent i = new Intent(this, RegisterActivity.class);
         startActivity(i);
+    }
+
+    public void login(View view){
+
+        EditText inputUsernameTemp = findViewById(R.id.inputUsername);
+        EditText inputPasswordTemp = findViewById(R.id.inputPassword);
+
+        if(inputUsernameTemp.getText().toString().equals(username) && inputPasswordTemp.getText().toString().equals(password) && !inputUsernameTemp.getText().toString().equals("") && !inputPasswordTemp.getText().toString().equals("")) {
+            Intent i = new Intent(this, HomeActivity.class);
+            startActivity(i);
+        } else if(!inputUsernameTemp.getText().toString().equals(username) || inputUsernameTemp.getText().toString().equals("")) {
+            Toast.makeText(getApplicationContext(),"Username not found.",Toast.LENGTH_SHORT).show();
+        } else if(!inputPasswordTemp.getText().toString().equals(password) || inputPasswordTemp.getText().toString().equals("")) {
+            Toast.makeText(getApplicationContext(),"Password incorrect.",Toast.LENGTH_SHORT).show();
+        }
+
     }
 
 }
