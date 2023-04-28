39ec8a679a9dc0bac795f995fedb7fa61c0f5b6b
Mus <21ftt1241@student.pb.edu.bn>
2023-04-28 13:43:39 +0800

    update the design

--- app/src/main/java/com/example/asg02/HomeActivity.java
+++ app/src/main/java/com/example/asg02/HomeActivity.java
@@ -2,7 +2,9 @@ package com.example.asg02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+    }
+    public void openOrderPage(View view){
+        Intent i = new Intent(this, OrderPage.class);
+        startActivity(i);
+    }
+
+    public void openOrderHistory(View view){
+        Intent i = new Intent(this, OrderHistoryPage.class);
+        startActivity(i);
     }
 }
