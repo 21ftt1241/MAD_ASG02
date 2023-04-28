96a787c12720fdadf4adfaebbea64a1a320c241a
Maxi <21FTT1245@student.pb.edu.bn>
2023-04-29 03:00:25 +0800

    Update for order page
    Logic added for order page. Order Summary yet to be finish.

--- app/src/main/java/com/example/asg02/OrderPageActivity.java
+++ app/src/main/java/com/example/asg02/OrderPageActivity.java
@@ -2,8 +2,11 @@ package com.example.asg02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class OrderPageActivity extends AppCompatActivity {
 
@@ -14,6 +17,128 @@ public class OrderPageActivity extends AppCompatActivity {
 
         TextView updateUsername = findViewById(R.id.wlcMsg);
         updateUsername.setText("Welcome, " + LoginActivity.username + "!");
+    }
+
+    public void confirmOrder(View view) {
+        Intent i = new Intent(this, OrderSummaryActivity.class);
+        startActivity(i);
+    }
+
+    TextView textview;
+    int number;
+
+    public void americano(View view) {
+
+        textview = findViewById(R.id.americanoNumber);
+        number = Integer.parseInt(textview.getText().toString());
+
+        switch(view.getId()) {
+            case R.id.americanoAdd: number++; break;
+            case R.id.americanoMinus: number--; break;
+            default: Toast.makeText(getApplicationContext(),"Something went wrong. Please try again",Toast.LENGTH_SHORT).show();
+        }
+
+        if(number < 0) {
+            textview.setText("0");
+        } else {
+            textview.setText(String.valueOf(number));
+        }
+
+    }
+
+    public void latte(View view) {
+
+        textview = findViewById(R.id.latteNumber);
+        number = Integer.parseInt(textview.getText().toString());
+
+        switch(view.getId()) {
+            case R.id.latteAdd: number++; break;
+            case R.id.latteMinus: number--; break;
+            default: Toast.makeText(getApplicationContext(),"Something went wrong. Please try again",Toast.LENGTH_SHORT).show();
+        }
+
+        if(number < 0) {
+            textview.setText("0");
+        } else {
+            textview.setText(String.valueOf(number));
+        }
+
+    }
+
+    public void cappuccino(View view) {
+
+        textview = findViewById(R.id.cappuccinoNumber);
+        number = Integer.parseInt(textview.getText().toString());
+
+        switch(view.getId()) {
+            case R.id.cappuccinoAdd: number++; break;
+            case R.id.cappuccinoMinus: number--; break;
+            default: Toast.makeText(getApplicationContext(),"Something went wrong. Please try again",Toast.LENGTH_SHORT).show();
+        }
+
+        if(number < 0) {
+            textview.setText("0");
+        } else {
+            textview.setText(String.valueOf(number));
+        }
+
+    }
+
+    public void green(View view) {
+
+        textview = findViewById(R.id.greenNumber);
+        number = Integer.parseInt(textview.getText().toString());
+
+        switch(view.getId()) {
+            case R.id.greenAdd: number++; break;
+            case R.id.greenMinus: number--; break;
+            default: Toast.makeText(getApplicationContext(),"Something went wrong. Please try again",Toast.LENGTH_SHORT).show();
+        }
+
+        if(number < 0) {
+            textview.setText("0");
+        } else {
+            textview.setText(String.valueOf(number));
+        }
+
+    }
+
+    public void oolong(View view) {
+
+        textview = findViewById(R.id.oolongNumber);
+        number = Integer.parseInt(textview.getText().toString());
+
+        switch(view.getId()) {
+            case R.id.oolongAdd: number++; break;
+            case R.id.oolongMinus: number--; break;
+            default: Toast.makeText(getApplicationContext(),"Something went wrong. Please try again",Toast.LENGTH_SHORT).show();
+        }
+
+        if(number < 0) {
+            textview.setText("0");
+        } else {
+            textview.setText(String.valueOf(number));
+        }
+
+    }
+
+    public void lemon(View view) {
+
+        textview = findViewById(R.id.lemonNumber);
+        number = Integer.parseInt(textview.getText().toString());
+
+        switch(view.getId()) {
+            case R.id.lemonAdd: number++; break;
+            case R.id.lemonMinus: number--; break;
+            default: Toast.makeText(getApplicationContext(),"Something went wrong. Please try again",Toast.LENGTH_SHORT).show();
+        }
+
+        if(number < 0) {
+            textview.setText("0");
+        } else {
+            textview.setText(String.valueOf(number));
+        }
+
     }
 
 }
